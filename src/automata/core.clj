a54f667cf6c8f670517388de17c3d9cfa38ea382
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2014-02-11 23:21:11 -0800

    allow free steps and start states.

--- src/automata/core.clj
+++ src/automata/core.clj
@@ -1,7 +1,8 @@
 (ns automata.core
   (:use [clojure.pprint]))
 
-(def foo [[1 2 \a]
+(def foo [[:start 1 :free]
+          [1 2 \a]
           [2 1 \b]])
 
 (def acceptingness {1 false
@@ -24,12 +25,24 @@
 (defn accepting-by-id [rules id-number]
   (acceptingness id-number))
 
+(defn free-by-id [rules id-number]
+  (= :free
+     (nth
+       (first (filter
+                  (fn [rule] (= (first rule) id-number))
+                  rules
+                  ))
+     2)))
+
 (accepting-by-id foo 1)
 
 (defn next-state [rules state input]
   (defn check-rule [rule]
             (and (= (id rule) state)
-                 (= (expected-char rule) input)))
+                 (or
+                    (= (expected-char rule) :free)
+                    (= (expected-char rule) input)
+                  )))
   (nth (first (filter check-rule rules)) 1))
 
 (next-state foo 1 \a)
@@ -37,16 +50,17 @@
 
 foo
 
-(defn run-dfa [rules state input & last_log]
+(defn run-dfa [rules input & [state last_log]]
   (if (not= (count input) 0)
-    (let [goto (next-state rules state (first input))
-          log (cons [(first input) goto] (or last_log (list [:start state])))]
+    (let [goto (next-state rules (or state :start) (first input))
+          log (cons [(first input) goto] (or last_log (list [:start state])))
+          remaining-input (if (free-by-id rules (or state :start)) input (rest input))]
       (if-not (nil? goto)
-        (recur rules goto (rest input) log)))
+          (recur rules remaining-input [goto log])))
     (list (accepting-by-id rules state) (reverse last_log))))
 
 (defn main []
-  (pprint (run-dfa foo 1 "ababababababababababababababababababa")))
+  (pprint (run-dfa foo "ababababababababababababababababababa")))
 
 (first "abab")
 (rest "abab")
